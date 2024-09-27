bc006acc20401415f7998be159d19fc9b1e99c80
VITOR FELIX <76710844+vgabrielk@users.noreply.github.com>
2024-09-27 18:46:47 -0300

    config: Setup inicial do projeto

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+      <div class="px-12 mt-5">
+
+  <router-view />
+  </div>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './index.css'
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
