4227bf66189b5aa6e12a874c6499f81af741d632
VITOR FELIX <76710844+vgabrielk@users.noreply.github.com>
2024-10-03 05:00:01 -0300

    chore: Ajuste no filtro + responsividade dos filtros no mobile

--- src/layouts/header/HeaderLayout.vue
+++ src/layouts/header/HeaderLayout.vue
@@ -1,52 +1,67 @@
 <template>
-  <header :class="[currentTheme.bg, currentTheme.borderBottom]" class="flex items-center justify-between px-6 py-4" >
-    <div class="flex items-center ">
+  <header :class="[currentTheme.bg, currentTheme.borderBottom]" class="flex items-center justify-between px-6 py-4">
+    <div class="flex items-center gap-4">
       <button class="text-gray-500 focus:outline-none lg:hidden" @click="isOpen = true">
         <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4 6H20M4 12H20M4 18H11" :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round"
-            stroke-linejoin="round" />
+          <path d="M4 6H20M4 12H20M4 18H11" :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round" />
         </svg>
       </button>
-
-      <div class="relative mx-4 lg:mx-0" v-if="route.name == 'pokedex'">
-        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-          <svg class="w-5 h-5 text-gray-500" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-        </span>
-
-        <input v-model="filterStore.filterQuery"
-          class="w-32 pl-10 pr-4 text-white-600 p-[6px] border-gray-200 rounded-md sm:w-64"
-          type="text" placeholder="Search">
-      </div>
-      <div class="ml-4" v-if="route.name == 'pokedex'">
-        <multi-select v-model="selectedTypes" @select="handleTypeChange"
-          class="w-32 sm:w-64 pl-2 pr-4 text-gray-900"
-          multiple
-           :options="pokemonTypeData"
-            label="name"
-            :taggable="true"
-             :hide-placeholder="true"
-              track-by="name"            >
-        <template #placeholder>
-
-        </template>
-        </multi-select>
-      </div>
-      <div class="ml-4" v-if="route.name == 'pokedex'" >
-        <button @click="onClearFilters">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path d="M2.5 2v6h6M21.5 22v-6h-6"/><path d="M22 11.5A10 10 0 0 0 3.2 7.2M2 12.5a10 10 0 0 0 18.8 4.2"/></svg>
-        </button>
-      </div>
     </div>
 
+ <div class="flex items-center gap-2">
+  <div class="flex items-center" v-if="route.name == 'pokedex'">
+      <div class="relative">
+        <button class="relative z-10 block w-8 h-8 overflow-hidden rounded-full  focus:outline-none"
+          @click="filterOpen = !filterOpen">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"   :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>
+        </button>
+        <div v-show="filterOpen" class="fixed inset-0 z-10 w-full h-full" @click="filterOpen = false"></div>
+        <transition enter-active-class="transition duration-150 ease-out transform"
+          enter-from-class="scale-95 opacity-0" enter-to-class="scale-100 opacity-100"
+          leave-active-class="transition duration-150 ease-in transform" leave-from-class="scale-100 opacity-100"
+          leave-to-class="scale-95 opacity-0">
+          <div v-show="filterOpen" class="absolute w-60 z-20 right-0 py-2 mt-2 rounded-md" :class="[currentTheme.bg]">
+            <div class="block px-4 py-2 text-sm text-gray-700 hover:text-white">
+              <input v-model="localSearch" :class="[currentTheme.text]"
+                class="w-auto pr-4 text-white-600 p-[6px] border-gray-200 rounded-md sm:w-full " type="text"
+                placeholder="Search">
+            </div>
+            <div class="block px-4 py-2 text-sm text-gray-700 hover:text-white">
+              <multi-select v-model="selectedTypes" class="w-full sm:w-full text-gray-900" multiple
+                :options="pokemonTypeData" label="name" :taggable="true" :hide-placeholder="true" track-by="name">
+                <template #placeholder>
+
+                </template>
+              </multi-select>
+            </div>
+            <div class="block px-4 py-2 text-sm text-gray-700 hover:text-white">
+              <button :class="[currentTheme.bg, currentTheme.text]" @click="applyFilter" class="rounded px-4 py-2">
+                <svg class="w-5 h-7 text-gray-500" viewBox="0 0 24 24" fill="none">
+                  <path
+                    d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                    :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </button>
+              <button :class="currentTheme.bg" @click="onClearFilters" class=" text-white rounded px-4 py-2 h-12">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none":stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path d="M2.5 2v6h6M21.5 22v-6h-6"/><path d="M22 11.5A10 10 0 0 0 3.2 7.2M2 12.5a10 10 0 0 0 18.8 4.2"/></svg>              </button>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
     <div class="flex items-center">
       <div class="relative">
         <button class="relative z-10 block w-8 h-8 overflow-hidden rounded-full  focus:outline-none"
           @click="dropdownOpen = !dropdownOpen">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            :stroke="isDarkMode ? '#ffffff' : '#111111'" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round">
+            <circle cx="12" cy="12" r="3"></circle>
+            <path
+              d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
+            </path>
+          </svg>
         </button>
 
         <div v-show="dropdownOpen" class="fixed inset-0 z-10 w-full h-full" @click="dropdownOpen = false"></div>
@@ -57,12 +72,14 @@
           leave-to-class="scale-95 opacity-0">
           <div v-show="dropdownOpen" class="absolute right-0 z-20 w-48 py-2 mt-2 rounded-md" :class="[currentTheme.bg]">
             <div class="block px-4 py-2 text-sm text-gray-700 hover:text-white">
-              <toogle-dark-mode-component/>
+              <toogle-dark-mode-component />
             </div>
           </div>
         </transition>
       </div>
     </div>
+    
+  </div>
   </header>
 </template>
 
@@ -75,31 +92,36 @@ import { useFilterStore } from "@/stores/filterStore";
 import { pokemonTypeData } from "@/utils/pokemon-type-data";
 import { useDarkMode } from "@/stores/darkModeStore";
 import { useRoute } from "vue-router";
-const {currentTheme, isDarkMode} = toRefs(useDarkMode());
+const { currentTheme, isDarkMode } = toRefs(useDarkMode());
 const { isOpen } = useSidebar();
+
+
+const localSearch = ref<string>();
+const selectedTypes = ref<string[]>([]);
 
 const route = useRoute();
 const filterStore = useFilterStore();
 const dropdownOpen = ref(false);
+const filterOpen = ref(false);
 
 const onClearFilters = () => {
   selectedTypes.value = []
+  localSearch.value = ''
   filterStore.clearFilters();
 }
 
-const selectedTypes = ref<string[]>([]);
 
-const handleTypeChange = () => {
-  console.log('chamou', selectedTypes.value)
-  filterStore.applyTypeFilter(selectedTypes.value);
-};
+const applyFilter = () => {
+
+  filterStore.applyFilter(localSearch.value)
+  filterStore.applyTypeFilter(selectedTypes.value)
+}
 </script>
 
 <style>
 @import 'vue-multiselect/dist/vue-multiselect.min.css';
 
-.multiselect__tags{
+.multiselect__tags {
   background-color: transparent;
 }
-
 </style>
--- src/pages/Pokedex.vue
+++ src/pages/Pokedex.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="mt-5">
-    <div v-if="filterNotResults && filteredPokemons.length == 0">
-      Nenhum resultado encontrado
+    <div v-if="isLoading">
+      <loading-component/>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+    <div v-else-if="filterNotResults && filteredPokemons.length === 0">
+      <p>
+        Nenhum resultado encontrado
+      </p>
+    </div>
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
       <div v-for="pokemon in filteredPokemons" :key="pokemon.id" @click="() => redirectToPokemonDetails(pokemon.id)" :class="[
-      
         'p-1 md:p-4 flex relative rounded cursor-pointer',
         currentTheme.bg
       ]">
-
         <div class="flex-col min-w-[100px]">
           <img :src="pokemon.sprites.other.showdown.front_default" alt="" class="w-14 h-14 mb-2" />
           <div class="mt-4 flex w-full ml-[-6px]">
@@ -20,105 +23,48 @@
         </div>
 
         <div class="flex flex-col">
-          <h1 :class="[currentTheme.text]" class="font-semibold flex items-center justify-start mx-0 px-0">{{ pokemon.id
-            }}</h1>
+          <h1 :class="[currentTheme.text]" class="font-semibold flex items-center justify-start mx-0 px-0">{{ pokemon.id }}</h1>
           <h2 :class="currentTheme.text" class="text-lg font-semibold capitalize">{{ pokemon.name }}</h2>
-
         </div>
-
       </div>
     </div>
-  </div>
 
-  <pagination-component
-    :total-pages="totalPages"
-    :current-page="currentPage"
-    @update:currentPage="updateCurrentPage"
-  />
+    <pagination-component
+      v-if="!isLoading && totalPages > 0"
+      :total-pages="totalPages"
+      :current-page="currentPage"
+      @update:currentPage="updateCurrentPage"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
-
 import PaginationComponent from '@/components/pagination/PaginationComponent.vue';
+import LoadingComponent from '@/components/loading/LoadingComponent.vue';
 import router from '@/router/routes';
-
-import { pokemonTypeMap } from '@/types/pokemon-type-map'
-import { computed, onMounted, onUnmounted, ref, toRefs } from 'vue';
+import { computed, onMounted, ref, toRefs, watch } from 'vue';
 import { pokeApiUrl } from '@/services/api';
 import { PokemonRequestDetail, PokemonRequestListResult } from '@/types/pokemon-request-types';
-import { useRoute } from 'vue-router';
 import { getPokemonTypeImage } from '@/utils/pokemon-get-type-image';
 import { notify } from '@kyvg/vue3-notification';
 import { useFilterStore } from '@/stores/filterStore';
 import { useDarkMode } from '@/stores/darkModeStore';
 
+const { currentTheme } = toRefs(useDarkMode());
 
-const {currentTheme, isDarkMode} = toRefs(useDarkMode());
-
-const route = useRoute();
-
-const showPagination = ref(false);
 const filterStore = useFilterStore();
 const currentPage = ref(1);
 const totalPages = ref(0);
 const pokemons = ref<PokemonRequestDetail[]>([]);
-const MAX_LIMIT = 1302;
-const DEFAULT_LIMIT = 20;
+const MAX_LIMIT = 1302; 
+const DEFAULT_LIMIT = 20; 
+const isLoading = ref(false); 
 const filterNotResults = ref(false);
 
 const paginationLimit = computed(() => {
-  return useFilterStore().paginationLimit
-})
-
-const redirectToPokemonDetails = (id: number) => {
-  router.push({
-    name: 'pokemon-details',
-    params: {
-      id: id
-    }
-  })
-}
-
-
-const handleScroll = () => {
-  const scrollPosition = window.innerHeight + window.scrollY;
-  const threshold = document.body.offsetHeight - 100;
-  if (scrollPosition >= threshold) {
-    showPagination.value = true;
-  } else {
-    showPagination.value = false;
-  }
-};
-
-const fetchPokemons = async (): Promise<void> => {
-  try {
-    const pokemonList = await fetchPokemonList();
-    const pokemonDetails = await fetchPokemonDetails(pokemonList);
-    pokemons.value = pokemonDetails;
-
-    if (!filterStore.selectedTypes.length && !filterStore.filterQuery) {
-      totalPages.value = Math.ceil(MAX_LIMIT / DEFAULT_LIMIT);
-    } else {
-      totalPages.value = 1;
-    }
-  } catch (error: any) {
-    const errorMessage = error.response?.data?.message || error.message || 'Erro ao carregar dados da API';
-    notify({ type: 'error', text: errorMessage });
-  }
-};
-
-const fetchPokemonList = async (): Promise<PokemonRequestListResult[]> => {
-  const activeLimit = paginationLimit; 
-  const response = await axios.get(`${pokeApiUrl}/pokemon?limit=${activeLimit}&offset=${(currentPage.value - 1) * DEFAULT_LIMIT}`);
-  return response.data.results;
-};
-
-const fetchPokemonDetails = async (pokemonList: PokemonRequestListResult[]): Promise<PokemonRequestDetail[]> => {
-  const detailsPromises = pokemonList.map((pokemon) => axios.get<PokemonRequestDetail>(pokemon.url));
-  const detailsResponses = await Promise.all(detailsPromises);
-  return detailsResponses.map((res) => res.data);
-};
+  return useFilterStore().paginationLimit;
+});
 
 const filteredPokemons = computed(() => {
   return pokemons.value.filter(pokemon => {
@@ -134,25 +80,81 @@ const filteredPokemons = computed(() => {
     const matchesType = filterStore.selectedTypes.length
       ? pokemon.types.some(item => filterStore.selectedTypes.includes(item.type.name.toLowerCase()))
       : true;
-      console.log(filterStore.selectedTypes)
 
     return matchesName && matchesId && matchesType;
   });
 });
+
+
+const redirectToPokemonDetails = (id: number) => {
+  router.push({
+    name: 'pokemon-details',
+    params: { id }
+  });
+};
+
+const fetchPokemons = async (): Promise<void> => {
+  isLoading.value = true; 
+  try {
+    const response = await axios.get(`${pokeApiUrl}/pokemon?limit=${DEFAULT_LIMIT}&offset=${(currentPage.value - 1) * DEFAULT_LIMIT}`);
+    const pokemonList = response.data.results;
+    const pokemonDetails = await fetchPokemonDetails(pokemonList);
+    pokemons.value = pokemonDetails;
+
+    totalPages.value = Math.ceil(pokemons.value.length / paginationLimit.value);
+    filterNotResults.value = pokemonDetails.length === 0; 
+  } catch (error: any) {
+    const errorMessage = error.response?.data?.message || error.message || 'Erro ao carregar dados da API';
+    notify({ type: 'error', text: errorMessage });
+  } finally {
+    isLoading.value = false; 
+  }
+};
+
+const fetchPokemonDetails = async (pokemonList: PokemonRequestListResult[]): Promise<PokemonRequestDetail[]> => {
+  const results: PokemonRequestDetail[] = [];
+  const limit = 5; 
+  const queue: Promise<void>[] | any = [];
+
+  for (const pokemon of pokemonList) {
+    const request = axios.get<PokemonRequestDetail>(pokemon.url)
+      .then(response => results.push(response.data));
+
+    queue.push(request);
+
+    if (queue.length >= limit) {
+      await Promise.all(queue);
+      queue.length = 0;
+    }
+  }
+
+  await Promise.all(queue);
+  return results;
+};
 
 const updateCurrentPage = (newPage: number) => {
   currentPage.value = newPage;
   fetchPokemons();
 };
 
+watch([() => filterStore.filterQuery, () => filterStore.selectedTypes], async () => {
+  isLoading.value = true; 
+  if (filterStore.filterQuery || filterStore.selectedTypes.length) {
+    const response = await axios.get(`${pokeApiUrl}/pokemon?limit=${MAX_LIMIT}`);
+    const pokemonList = response.data.results;
+    const pokemonDetails = await fetchPokemonDetails(pokemonList);
+    pokemons.value = pokemonDetails;
+
+    totalPages.value = Math.ceil(filteredPokemons.value.length / paginationLimit.value);
+    currentPage.value = 1; 
+    filterNotResults.value = pokemonDetails.length === 0; 
+  } else {
+    fetchPokemons();
+  }
+  isLoading.value = false; 
+});
 
 onMounted(fetchPokemons);
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll);
-});
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll);
-});
 </script>
 
 <style scoped></style>
