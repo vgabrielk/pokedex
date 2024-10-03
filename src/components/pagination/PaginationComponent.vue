f2c9d0bfe2c9581c78e98a495b00a0ce483a72cd
VITOR FELIX <76710844+vgabrielk@users.noreply.github.com>
2024-10-03 06:22:23 -0300

    fix: Ajuste no componente de paginação

--- src/components/pagination/PaginationComponent.vue
+++ src/components/pagination/PaginationComponent.vue
@@ -1,51 +1,49 @@
 <template>
-    <div
-      class="p-4"
-    >   
-      <button
+  <div class="p-4">
+    <button
       class="btn-default rounded-tl-md rounded-bl-md border-r-[#e6e6e627] border-r cursor-pointer"
       @click="goToPrevious"
-        :disabled="currentPage === totalPages"
-      >
-        Prev
-      </button>
-      <button
-        class="btn-default rounded-tr-md rounded-br-md cursor-pointer"
-        @click="goToNext"
-        :disabled="currentPage === 1"
-      >
-        Next
-      </button>
-      
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, defineProps } from 'vue';
-  
-  const props = defineProps<{
-    totalPages: number;
-    currentPage: number;
-  }>();
+      :disabled="currentPage === 1"
+    >
+      Prev
+    </button>
 
-  const emit = defineEmits();
+    <button
+      class="btn-default rounded-tr-md rounded-br-md cursor-pointer"
+      @click="goToNext"
+      :disabled="currentPage === totalPages"
+    >
+      Next
+    </button>
+  </div>
+</template>
 
-  
-  const goToPrevious = () => {
-    if (props.currentPage > 1) {
-      emit('update:currentPage', props.currentPage - 1);
-    }
-  };
-  
-  const goToNext = () => {
-    if (props.currentPage < props.totalPages) {
-      emit('update:currentPage', props.currentPage + 1);
-    }
-  };
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
 
-  
-  </script>
-  
-  <style scoped>
-  </style>
-  
+const props = defineProps<{
+  totalPages: number;
+  currentPage: number;
+}>();
+
+
+const emit = defineEmits<{
+  (e: 'update:currentPage', page: number): void;
+}>();
+
+const goToPrevious = () => {
+  if (props.currentPage > 1) {
+    emit('update:currentPage', props.currentPage - 1);
+  }
+};
+
+const goToNext = () => {
+  console.log('asdas')
+  if (props.currentPage < props.totalPages) {
+    emit('update:currentPage', props.currentPage + 1);
+  }
+};
+</script>
+
+<style scoped>
+</style>
